0e0c16b052cd5797c02fc1d0bd5add3c8c52db3f
iF4res <44025576+iF4res@users.noreply.github.com>
2018-10-10 22:02:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,3 +7,21 @@ client.login(process.env.BOT_TOKEN);
 client.on('ready', () => { 
   console.log('Hi')
 });
+
+
+client.on('message', message => {
+    let log = message.guild.channels.find('name', 'log');
+    let reason = message.content.split(" ").slice(2).join(' ');
+    let p = message.mentions.members.first();
+    if(message.content.startsWith(prefix + "warn")){
+        if(!p) return message.reply(`Mention a User!`);
+        if(reason.length < 1) return message.reply(`Set a reason!`)
+        var embed = new Discord.RichEmbed()
+        .setTitle(`New Warning!`)
+        .addField(`For`, `<@${p.user.id}`)
+        .addField(`By`, `<@${message.author.id}>`)
+        .addField(`Reason`, reason)
+        .addField(`In Chat`, `<#${message.channel.id}>`)
+        log.send({embed})
+    }
+});
