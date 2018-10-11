c738cb72a7ec2171011cf3f8f4f4fca259cddf18
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-11 14:35:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ client.on('message', message => {
     let reason = message.content.split(" ").slice(2).join(' ');
     let p = message.mentions.members.first();
     if(message.content.startsWith(prefix + "warn")){
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`**:x: | This Command is Just for Adminstration**`);
         if(!p) return message.reply(`Mention a User!`);
         if(reason.length < 1) return message.reply(`Set a reason!`)
         var embed = new Discord.RichEmbed()
@@ -24,8 +25,54 @@ client.on('message', message => {
         .addField(`By`, `<@${message.author.id}>`)
         .addField(`Reason`, reason)
         .addField(`In Chat`, `<#${message.channel.id}>`)
+        .setColor("WHITE")
           message.delete();
         message.channel.send(`${p} ` + reason)
+        log.send({embed})
+    }
+});
+
+
+client.on('message', message => {
+    let log = message.guild.channels.find('name', 'log');
+    let reason = message.content.split(" ").slice(2).join(' ');
+    let p = message.mentions.members.first();
+    if(message.content.startsWith(prefix + "ban")){
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`**:x: | This Command is Just for Adminstration**`);
+        if(!p) return message.reply(`Mention a User!`);
+        if(reason.length < 1) return message.reply(`Set a reason!`)
+        var embed = new Discord.RichEmbed()
+        .setTitle(`New Ban!`)
+        .addField(`For`, `<@${p.user.id}>`)
+        .addField(`By`, `<@${message.author.id}>`)
+        .addField(`Reason`, reason)
+        .addField(`In Chat`, `<#${message.channel.id}>`)
+        .setColor("RED")
+          message.delete();
+        message.channel.send(`<@${message.author.id}>, **Done**`)
+        log.send({embed})
+    }
+});
+
+
+client.on('message', message => {
+    let log = message.guild.channels.find('name', "log");
+    let reason = message.content.split(" ").slice(2).join(' ');
+    let p = message.mentions.members.first();
+    if(message.content.startsWith(prefix + "mute")){
+        if(!p) return message.reply(`Mention a User!`);
+        if(reason.length < 1) return message.reply(`Set a reason!`)
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`**:x: | This Command is Just for Adminstration**`);
+        if(p.roles.find('name', "Muted")) return message.reply(`**تم اسكات هذا العضو مسبقاً**`)
+        var embed = new Discord.RichEmbed()
+        .setTitle(`New Ban!`)
+        .addField(`For`, `<@${p.user.id}>`)
+        .addField(`By`, `<@${message.author.id}>`)
+        .addField(`Reason`, reason)
+        .addField(`In Chat`, `<#${message.channel.id}>`)
+        .setColor("RED")
+          message.delete();
+        message.channel.send(`**تم حفظ السبب وستتم مراجعته من قبل المسؤولين**`)
         log.send({embed})
     }
 });
