2ba052ba4e4b63b90f4166fd174d783c9b47503b
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-12 23:10:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,7 +117,7 @@ client.on('message', message => {
         var embed = new Discord.RichEmbed()
         .setTitle(`ClickHere`)
         .setDescription(`PalestineBot Support Server`)
-        .setURL("https://discord.gg/yRHuwq")
+        .setURL("https://discord.gg/JvcKRKk")
         .setColor("BLACK")
         .setTimestamp()
         .setFooter(" ")
@@ -172,7 +172,32 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('No Pe
            });
 
 
-client.on('message', message => {
+ client.on('message', message => {
+    if (message.content.startsWith(prefix + 'clear')) {
+      if(!message.channel.guild) return;
+  if (!message.member.roles.find('name', 'اسم الرتبة الي يمديها تسوي كلير')) return ;
+      
+  message.delete()
+  if(!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1);
+  
+  const messagecount = parseInt(args.join(' '));
+  
+  message.channel.fetchMessages({
+  
+  limit: messagecount
+  
+  }).then(messages => message.channel.bulkDelete(messages));
+  message.channel.sendMessage("", {embed: {
+    title: "``✏️✅ تــم مسح الشات ``",
+    color: 0x06DF00,
+    footer: {
+    
+    }
+    }}).then(msg => {msg.delete(3000)});
+  };
+  
+  });.on('message', message => {
     let args = message.content.split(" ").slice(1);
 if (message.content.startsWith(prefix + 'clear')) {
  let args = message.content.split(" ").slice(1)
