feef10441779b95d42099951d60c01944688c013
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-13 00:03:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,37 @@ client.on('ready', () => {
 client.on('message', message => {
   if(message.content.startsWith(prefix + 'help')){
     var embed = new Discord.RichEmbed()
-    .setTitle(`${client.user.username}#${client.user.discrimination}'s Help`)
-    .addField('${prefix}help', `**To Get the help List**`)
-    .addField('${prefix}warn', `**To Warn a user**`)
+    .setTitle(`${client.user.username}#${client.user.discrimation}'s Help`)
+    .addField(`${prefix}help`, `**To Get the help List**`)
+    .addField(`${prefix}warn`, `**To Warn a user**`)
     .setColor("RANDOM")
     .setTimestamp()
     .setFooter(" ")
     message.channel.send({embed})
   }
+});
+
+
+client.on('message', message => {
+    let log = message.guild.channels.find('name', 'log');
+    let reason = message.content.split(" ").slice(2).join(' ');
+    let p = message.mentions.members.first();
+    if(message.content.startsWith(prefix + "warn")){
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply(`**❌ | This Command is Just for Adminstration**`);
+            message.delete();
+        if(!p) return message.reply(`Mention a User!`);
+        if(reason.length < 1) return message.reply(`Set a reason!`)
+        var embed = new Discord.RichEmbed()
+        .setTitle(`New Warning!`)
+        .addField(`For`, `<@${p.user.id}>`)
+        .addField(`By`, `<@${message.author.id}>`)
+        .addField(`Reason`, reason)
+        .addField(`In Chat`, `<#${message.channel.id}>`)
+        .setColor("WHITE")
+        .setTimestamp()
+        .setFooter(" ")
+            message.channel.send(`${p} ` + reason)
+            message.delete();
+        log.send({embed})
+    }
 });
