0d66457bbf040f9f51e3a0049699529afede2292
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-12 17:23:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => { 
   console.log('Loggen in As ${client.user.username}')
- client.user.setGame(`PalestineBot | ${client.guild.size}/25`, "https://twitch.tv/F4res")
+ client.user.setGame(`PalestineBot | ${client.guilds.size}/25`, "https://twitch.tv/F4res")
  client.user.setStatus(`Idel`)
 });
 
