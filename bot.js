b0eabc07f95232a51028f63bc70935394e8229a0
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-15 17:38:57 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -190,3 +190,17 @@ client.on('message', message => {
     message.channel.send({embed})
   }
 });
+
+
+client.on('message', message => {
+    if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith(prefix + 'bcall')){
+if (message.author.id !== '483033515026284564') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+if(!message.author.id === '483033515026284564') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
