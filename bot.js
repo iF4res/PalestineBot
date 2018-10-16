93e7a74562c0c6d88465a17e1446513e8a3d079b
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-16 21:34:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@ const Discord = require('discord.js');
 const prefix = "$";
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN);
+client.login('NTAwMDM4NzA1NjA0NDYwNTU1.DqeznQ.6uyKIoU9jTF2wwx0RxxjVGaHG78');
 
 client.on('ready', () => { 
-  console.log('Loggen in As ${client.user.username}')
+  console.log(`Loggen in As ${client.user.username}`)
  client.user.setGame(`PalestineBot | ${client.guilds.size}/25`, "https://twitch.tv/F4res")
  client.user.setStatus(`Idel`)
 });
