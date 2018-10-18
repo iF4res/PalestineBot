6ca992ede6f3525b3d9e6b7a5440434d9f84bf32
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-18 22:02:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,24 +12,24 @@ client.user.setStatus('Idel')
 
 
 client.on('message', message => {
-    let log = message.guild.channels.find('name', "log");
+    let log = message.guild.channels.find('name', 'log');
     let reason = message.content.split(" ").slice(2).join(' ');
     let p = message.mentions.members.first();
     if(message.content.startsWith(prefix + "ban")){
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(`:x: | This COmmand is just for ADMINSTRATION`);
+      if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply(`You Don't have **Ban_Members** Permission!`);
             message.delete();
         if(!p) return message.reply(`Mention a User!`);
-        if(!reason.length < 1) return message.reply(`Set a reason!`);
-        if(reason.includes('0')) reason = reason.replace('0', '**نشر سيرفرات الخاص**'); 
-        if(reason.includes('1')) reason = reason.replace('1', '**سب في الرومات الصوتيه**');
-        if(reason.includes('2')) reason = reason.replace('2', '**استخدام برامج تغيير صوت**');
-        if(reason.includes('3')) reason = reason.replace('3', '**ازعاج الاداره والاعضاء في الرومات العامه**');
-        if(reason.includes('4')) reason = reason.replace('4', '**سب الشخص مسبات قويه ولم ياخذ ميوت**');
-        if(reason.includes('5')) reason = reason.replace('5', '**اسم مسيء - غير لائق**');
-        if(reason.includes('6')) reason = reason.replace('6', '**سب الاهل**');
-        if(reason.includes('7')) reason = reason.replace('7', '**صوره غير لائقه**');
-        if(reason.includes('8')) reason = reason.replace('8', '**عنصريه**');
-        if(reason.includes('9')) reason = reason.replace('9', '**دخول باكثر من حساب ومو متبند**');
+        if(reason.length < 1) return message.reply(`Set a reason!`);
+    if(reason.includes('0')) reason = reason.replace('0', '**نشر سيرفرات الخاص**'); 
+    if(reason.includes('1')) reason = reason.replace('1', '**سب في الرومات الصوتيه**');
+    if(reason.includes('2')) reason = reason.replace('2', '**استخدام برامج تغيير صوت**');
+    if(reason.includes('3')) reason = reason.replace('3', '**ازعاج الاداره والاعضاء في الرومات العامه**');
+    if(reason.includes('4')) reason = reason.replace('4', '**سب الشخص مسبات قويه ولم ياخذ ميوت**');
+    if(reason.includes('5')) reason = reason.replace('5', '**اسم مسيء - غير لائق**');
+    if(reason.includes('6')) reason = reason.replace('6', '**سب الاهل**');
+    if(reason.includes('7')) reason = reason.replace('7', '**صوره غير لائقه**');
+    if(reason.includes('8')) reason = reason.replace('8', '**عنصريه**');
+    if(reason.includes('9')) reason = reason.replace('9', '**دخول باكثر من حساب ومو متبند**');
         var embed = new Discord.RichEmbed()
         .setTitle(`New Ban!`)
         .addField(`For`, `<@${p.user.id}>`)
