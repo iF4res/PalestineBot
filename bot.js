07915605797c19e065dc2f7a98f9857f8e598e55
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-19 18:07:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -60,6 +60,7 @@ client.on('message', message => {
 
 client.on('message', message => {
     if(message.content.startsWith(prefix + "server")){
+        if(!message.member.hasPermission('ADMINSTRATION')) return message.reply(`:x: | This Command is Just For Adminstration`);
         var embed = new Discord.RichEmbed();
         embed.setAuthor(`${message.guild.name}`, message.guild.iconURL)
         embed.addField(`:id: **Server ID**`, `${message.guild.id}`)
@@ -77,61 +78,30 @@ client.on('message', message => {
 });
 
 
-client.on("message", (message) => {
-    /// Codes Development.
-   if (message.content.startsWith("#ticket")) {     /// Codes Development.
-        const reason = message.content.split(" ").slice(1).join(" ");     /// Codes Development.
-        if (!message.guild.roles.exists("name", "◆ Support ◆")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// Codes Development.
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "◆ Support ◆");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });    /// Codes Development.
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor(0xCF40FA)
-                .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Staff** will be here soon to help.`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error);
+client.on('message', message => {
+    if (message.content.startsWith(prefix + 'clear')) {
+      if (!message.member.roles.find('name', "◆ Support ◆")) return message.reply(`No **◆ Support ◆** Role Find!`).catch(console.error);
+  message.delete()
+  if(!message.channel.guild) return;
+  let args = message.content.split(" ").slice(1);
+  
+  const messagecount = parseInt(args.join(' '));
+  
+  message.channel.fetchMessages({
+  
+  limit: messagecount
+  
+  }).then(messages => message.channel.bulkDelete(messages));
+  message.channel.sendMessage("", {embed: {
+    title: `\`\`\`Cleared ${args} Messages! .\`\`\``,
+    color: 0x06DF00,
+    footer: {
+    
     }
- 
- 
-  if (message.content.startsWith("#close")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
-            .then((m) => {
-                message.channel.awaitMessages(response => response.content === '#confirm', {
-                        max: 1,
-                        time: 10000,
-                        errors: ['time'],
-                    })    /// Codes Development.
-                    .then((collected) => {
-                        message.channel.delete();
-                    })    /// Codes Development.
-                    .catch(() => {
-                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
-                            m2.delete();
-                        }, 3000);
-                    });
-            });
-    }
- 
-});
+    }}).then(msg => {msg.delete(3000)});
+  };
+  
+  });
 ///general
 ///admins
 client.on('message', message => {
