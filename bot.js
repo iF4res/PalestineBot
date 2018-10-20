21ac31eb852894840c5e85b522514a08dd45e15b
F4res <44025576+iF4res@users.noreply.github.com>
2018-10-20 10:24:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,12 +78,9 @@ client.on('message', message => {
 });
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+client.on('message', message => {
 
-           client.on('message', message => {
-
-           if (message.content.startsWith(prefix + "id")) {
+           if (message.content.startsWith(prefix + "user")) {
                      if(!message.channel.guild) return message.reply(`هذا الأمر فقط ل السيرفرات :x:`);
 
                 message.guild.fetchInvites().then(invs => {
